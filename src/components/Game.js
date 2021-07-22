e5d066e7375553b93f2585ac3aa44da10c5524df
mpmcgee <mpmcgee@iu.edu>
2021-07-22 12:40:46 -0400

    added game status

--- src/components/Game.js
+++ src/components/Game.js
@@ -9,30 +9,48 @@ class Game extends React.Component {
     randomNumberCount: PropTypes.number.isRequired,
   };
   state = {
-    selectedNumbers: [],
+    selectedIds: [],
   };
 
   randomNumbers = Array.from({ length: this.props.randomNumberCount }).map(
     () => 1 + Math.floor(10 * Math.random())
   );
-  selectNumber = (numberIndex) => {
-    this.setState((prevState) => ({
-    selectedNumbers: [...prevState.selectedNumbers, numberIndex],
-    }));
-  };
+  
+  // game status
+  gameStatus = () => {
+    const sumSelected = this.state.selectedIds.reduce((acc, curr) => {
+      return acc + this.randomNumbers[curr];
+    }, 0);
+    if (sumSelected < this.target) {
+      return 'PLAYING';
+    }
+    if (sumSelected === this.target) {
+      return 'WON';
+    }
+    if (sumSelected > this.target) {
+      return 'LOST';
+    }
+  }
   target = this.randomNumbers
     .slice(0, this.props.randomNumberCount - 2)
     .reduce((acc, curr) => acc + curr, 0);
   // TODO: Shuffle the random numbers
 
   isNumberSelected = (numberIndex) => {
-    return this.state.selectedNumbers.indexOf(numberIndex) >= 0;
+    return this.state.selectedIds.indexOf(numberIndex) >= 0;
   }
 
+  selectNumber = (numberIndex) => {
+    this.setState((prevState) => ({
+    selectedIds: [...prevState.selectedIds, numberIndex],
+    }));
+  };
+  
   render() {
+    const gameStatus = this.gameStatus();
     return (
       <View style={styles.container}>
-        <Text style={styles.target}>{this.target}</Text>
+        <Text style={[styles.target, styles[`STATUS_${gameStatus}`]]}>{this.target}</Text>
         <View style={styles.randomContainer}>
           {this.randomNumbers.map((randomNumber, index) => (
             // <Text style={styles.random} key={index}>{randomNumber}</Text>
@@ -45,6 +63,7 @@ class Game extends React.Component {
           />
           ))}
         </View>
+        <Text>{gameStatus}</Text>
       </View>
     );
   }
@@ -66,6 +85,16 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-around',
+  },
+
+  STATUS_PLAYING: { 
+    backgroundColor: '#bbb',
+  },
+  STATUS_WON: { 
+    backgroundColor: 'green',
+  },
+  STATUS_LOST: { 
+    backgroundColor: 'red',
   },
 });
 
